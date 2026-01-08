d3d433e62d41afcbd9f1c9bd875a25ffdfa15804
sencheele <sencheele@gmail.com>
2026-01-08 19:18:23 +0300

    FEAT: показ карточки "товары закончились"

--- src/scripts/init-show-more.js
+++ src/scripts/init-show-more.js
@@ -1,6 +1,8 @@
 import { renderProducts } from './render-products'
 
 export const initShowMore = (allProductsList, container, showMore, step = 7) => {
+  const emptyCard = document.querySelector('[data-empty-card]')
+  
   let displayedCount = 0
 
   const renderNextBatch = () => {
@@ -11,9 +13,13 @@ export const initShowMore = (allProductsList, container, showMore, step = 7) => 
     renderProducts(container, nextProductsList)
     displayedCount += nextProductsList.length
 
+    if (emptyCard) container.appendChild(emptyCard)
+
     if (displayedCount >= allProductsList.length) {
       showMore.removeEventListener('click', renderNextBatch)
       showMore.classList.add('is-hidden')
+
+      if (emptyCard) emptyCard.classList.remove('is-hidden')
     }
   }
 
