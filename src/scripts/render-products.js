c4f4ce3c30f369cc3a5f4d1a547f3fdbe6958064
sencheele <sencheele@gmail.com>
2026-01-10 23:37:35 +0300

    FEAT: добавлена поддержка разных шаблонов карточек товара (product / cart) в renderProducts

--- src/scripts/render-products.js
+++ src/scripts/render-products.js
@@ -1,7 +1,20 @@
 import { createProductCard } from './create-product-card'
+import { createCartCard } from './create-cart-card'
 
-export const renderProducts = (container, productsList) => {
+export const renderProducts = (container, productsList, templateCard = 'product') => {
+  container.innerHTML = ''
   const fragment = document.createDocumentFragment()
-  productsList.forEach(product => fragment.append(createProductCard(product)))
+
+  switch (templateCard) {
+    case 'cart':
+      productsList.forEach(product => fragment.append(createCartCard(product)))
+      break
+    case 'product':
+      productsList.forEach(product => fragment.append(createProductCard(product)))
+      break
+    default:
+      break
+  }
+
   container.append(fragment)
 }
