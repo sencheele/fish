0b820b96790d42a54d6c11db487262146251d52e
sencheele <sencheele@gmail.com>
2026-01-17 16:46:57 +0300

    REFACTOR: сделать initStorageToggle более универсальной - Функция теперь принимает контейнер вместо селектора - Добавлен параметр для выполнения действия после изменения хранилища

--- src/scripts/init-storage-toggle.js
+++ src/scripts/init-storage-toggle.js
@@ -1,23 +1,24 @@
-export const initStorageToggle = (dataName, datasetName, storageKey) => {
-  const catalogContainer = document.querySelector('[data-catalog-list]')
-  if (!catalogContainer) return
-
-  let productItems = JSON.parse(localStorage.getItem(storageKey)) || []
-
-  catalogContainer.addEventListener('click', (event) => {
-    const button = event.target.closest(`[${dataName}]`)
+export const initStorageToggle = (containerEl, buttonSelector, storageKey, onChange) => {
+  containerEl.addEventListener('click', (e) => {
+    const button = e.target.closest(buttonSelector)
     if (!button) return
 
-    const id = Number(button.dataset[datasetName])
-    const isActive = !productItems.includes(id)
+    let productItems = JSON.parse(localStorage.getItem(storageKey)) || []
 
-    if (isActive) {
+    const id = Number(button.dataset.id)
+    const isAdded = !productItems.includes(id)
+
+    if (isAdded) {
       productItems.push(id)
     } else {
       productItems = productItems.filter(item => item !== id)
     }
 
-    button.classList.toggle('is-active', isActive)
+    // button.classList.toggle('is-active', isActive)
     localStorage.setItem(storageKey, JSON.stringify(productItems))
+
+    if (onChange) {
+      onChange()
+    }
   })
 }
